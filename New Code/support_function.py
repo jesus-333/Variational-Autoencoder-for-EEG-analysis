62b9432c95c82f1185411df52c79105740dbc1d4
jesus-333 <albi25.zanca@gmail.com>
2023-05-29 19:34:52 +0200

    Create support function files

---
+++ New Code/support_function.py
@@ -0,0 +1,42 @@
+"""
+@author: Alberto Zancanaro (Jesus)
+@organization: University of Padua (Italy)
+
+Minor support function used in the various script
+"""
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#%% Imports
+
+from torch import nn
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def get_activation(activation_name: dict):
+    """
+    Receive a string and return the relative activation function in pytorch.
+    Implemented for relu, elu, selu, gelu
+    """
+
+    if activation_name.lower() == 'relu':
+        return nn.ReLU()
+    elif activation_name.lower() == 'elu':
+        return nn.ELU()
+    elif activation_name.lower() == 'selu':
+        return nn.SELU()
+    elif activation_name.lower() == 'gelu':
+        return nn.GELU()
+    else:
+        error_message = 'The activation must have one of the following string: relu, elu, selu, gelu'
+        raise ValueError(error_message)
+
+def get_dropout(p_dropout : float, use_droput_2d : bool):
+    if use_droput_2d:
+        return nn.Dropout2d(p_dropout)
+    else: 
+        return nn.Dropout(p_dropout)
+
+def count_trainable_parameters(layer):
+    n_paramters = sum(p.numel() for p in  layer.parameters() if p.requires_grad)
+    return n_paramters
+
