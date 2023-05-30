c35c62972dc4a3a0dfb645e001f24b4873de4bc9
Jesus <albi25.zanca@gmail.com>
2023-05-30 15:25:56 +0200

    Work on vEEGNet and decoder

--- New Code/Decoder.py
+++ New Code/Decoder.py
@@ -19,34 +19,70 @@ class EEGNet_Decoder(nn.Module):
 
     def __init__(self, config : dict):
         super().__init__()
-
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+        # Parameter used during the creation of the networks
         use_bias = config['use_bias']
         D = config['D']
         activation = support_function.get_activation(config['activation'])
-        dropout = support_function.get_activation(config['p_dropout'], config['use_dropout_2d'])
+        dropout = support_function.get_activation(config['prob_dropout'], config['use_dropout_2d'])
 
-        
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+        # Feed-Forward section of the decoder
+
         # After the convolution layer reshape the array to this dimension
         self.dimension_reshape = config['dimension_reshape']
+
+        # Compute the number of neurons needed to obtain the shape defined by dimension_reshape
+        n_ouput_neurons = self.dimension_reshape.numel()
         
+        # Defined feed-forward encoder
+        self.ff_decoder = nn.Sequential(
+            nn.Linear(config['hidden_space'], n_ouput_neurons),
+            activation,
+        )
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+        # Convolutional section of the decoder
+
         self.separable_convolution_transpose = nn.Sequential(
             dropout,
-            nn.Upsample(),
+            nn.Upsample(scale_factor = config['p_kernel_2']),
             activation,
             nn.BatchNorm2d(config['filter_2']),
             nn.ConvTranspose2d(config['filter_1'] * D, config['filter_2'], kernel_size = (1, 1), groups = 1, bias = use_bias),
-            nn.ConvTranspose2d(config['filter_1'] * D, config['filter_1'] * D, kernel_size = config['c_kernel_3'], groups = config['filter_1'] * D, padding = 'same', bias = use_bias),
+            nn.ConvTranspose2d(config['filter_1'] * D, config['filter_1'] * D, kernel_size = config['c_kernel_3'], groups = config['filter_1'] * D, bias = use_bias),
         )
 
         self.spatial_convolution_transpose = nn.Sequential(
-
+            dropout,
+            nn.Upsample(scale_factor = config['p_kernel_1']),
+            activation,
+            nn.BatchNorm2d(config['filter_1'] * D),
+            nn.ConvTranspose2d(config['filter_1'], config['filter_1'] * D, kernel_size = config['c_kernel_2'], groups = config['filter_1'], bias = use_bias),
         )
 
-        self.temporal_convolution_transpose = nn.Sequential()
+        self.temporal_convolution_transpose = nn.Sequential(
+            nn.BatchNorm2d(config['filter_1']),
+            nn.Conv2d(1, config['filter_1'], kernel_size = config['c_kernel_1'], bias = use_bias),
+        )
         
     def forward(self, z):
-        return z
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        # Feed-Forward section
+        
+        # Feed-Forward decoder
+        x = self.ff_decoder(z)
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Other function
+        # Reshape the output for the convolutional section
+        x = torch.reshape(x, self.shape_input_conv_decoder)
+        
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        # Convolutional section
+
+        x = self.separable_convolution_transpose(x)
+        x = self.spatial_convolution_transpose(x)
+        x = self.temporal_convolution_transpose(x)
+
+        return x
+
 
--- New Code/vEEGNet.py
+++ New Code/vEEGNet.py
@@ -11,7 +11,7 @@ Implementation of vEEGNet model using PyTorch
 import torch
 from torch import nn
 
-import EEGNet, MBEEGNet
+import EEGNet, MBEEGNet, Decoder
 import config_model
 
 """
@@ -21,12 +21,18 @@ import config_model
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-class vEEGnet(nn.Module):
+class vEEGNet(nn.Module):
 
     def __init__(self, config : dict):
         super().__init__()
         
+        # Information used during network creation
+        
+        # Get hidden space dimension
         self.hidden_space = config['hidden_space']
+        
+        # Get the size and the output shape after an input has been fed into the encoder
+        n_input_neurons, decoder_ouput_shape = self.compute_number_of_neurons(config['C'], config['T'])
 
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
         # Create Encoder
@@ -40,32 +46,73 @@ class vEEGnet(nn.Module):
             raise ValueError("type_encoder must be 0 (EEGNET) or 1 (MBEEGNet)")
         
         # Feed-Forward section
-        n_input_neurons = self.compute_number_of_neurons(config['C'], config['T'])
         self.ff_encoder_mean = nn.Linear(n_input_neurons, self.hidden_space)
         self.ff_encoder_std = nn.Linear(n_input_neurons, self.hidden_space)
         
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
         # Create Decoder
-
+        # Note that the config used for the encoder  are also used for the decoder
+        
+        # Information specific for the creation of the decoder
+        config['encoder_config']['dimension_reshape'] = decoder_ouput_shape
+        config['encoder_config']['hidden_space'] = self.hidden_space
+        
+        # For the decoder we use the same type of the encoder
+        # E.g. if the encoder is EEGNet also the decoder will be EEGNet
+        if config["type_encoder"] == 0:
+            self.decoder = Decoder.EEGNet_Decoder(config['encoder_config']) 
+        if config["type_encoder"] == 1:
+            # TODO Implement MBEEGNet decoder 
+            self.decoder = MBEEGNet(config['encoder_config']) 
+        else:
+            raise ValueError("type_encoder must be 0 (EEGNET) or 1 (MBEEGNet)")
         # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
         # Other
 
 
     def forward(self, x):
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -                 
+        # Encoder section
         x = self.encoder(x)
         z_mean = self.ff_encoder_mean(x)
-        z_std = self.ff_encoder_std(x)
+        z_log_var = self.ff_encoder_std(x)
 
-        return x
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+        # Reparametrization
 
-    def compute_number_of_neurons(self, C, T):
+        z = self.reparametrize(z_mean, z_log_var)
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+        # Decoder
+
+        x_r = self.decoder(z)
+
+        return x, x_r, z_mean, z_log_var
+
+    def reparametrize(self, mu, log_var):
+        """
+        Reparametrization Trick to allow gradients to backpropagate from the stochastic part of the model
+        mu = Mean of the laten gaussian
+        log_var = logartim of the variance of the latent guassian
+        """
+        
+        sigma = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(sigma)
+        eps = eps.type_as(mu) # Setting z to be cuda when using GPU training 
+        
+        return mu + (sigma * eps)
+
+    def decoder_shape_info(self, C, T):
         """
         Compute the total number of neurons for the feedforward layer
+        Compute the shape of the input after pass through the convolutional encoder
+
+        Note that the computation are done for an input with batch size = 1
         """
 
         x = torch.rand(1, 1, C, T)
         x = self.cnn_encoder(x)
         input_neurons = len(x.flatten())
 
-        return input_neurons
+        return input_neurons, list(x.shape)
