7020c6ef7ba2d882ee2d46fde905556ff3495b08
jesus-333 <albi25.zanca@gmail.com>
2023-06-26 01:15:54 +0200

    Add loss functions (reconstruction, classification, vEEGNet)

--- New Code/loss_function.py
+++ New Code/loss_function.py
@@ -10,12 +10,19 @@ List of loss function used during training.
 #%% Imports
 
 import torch
+import torch.nn.functional as F
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Classifier loss
+
+def classifier_loss(true_label, predicted_label):
+    return F.nll_loss(predicted_label, true_label)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # Reconstruction loss
 
 def recon_loss_function(x, x_r):
-    pass
+    return F.mse_loss(x, x_r)
 
 def recon_loss_frequency_function(x, x_r):
     pass
@@ -33,7 +40,7 @@ def kl_loss_function(mu_1, log_var_1, mu_2, log_var_2):
     pass
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Variational autencoder loss
+# VAE loss
 
 def vae_loss_function(x, x_r, mu, log_var, config):
     if config['recon_loss_type'] == 0:
@@ -46,4 +53,20 @@ def vae_loss_function(x, x_r, mu, log_var, config):
     kl_loss = kl_loss_normal_function(mu, log_var)
 
     return config['alpha'] * recon_loss + config['beta'] * kl_loss, recon_loss, kl_loss
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def vEEGNet_loss(x, x_r, mu, log_var, true_label, predicted_label, config):
+    # Compute the loss of the VAE
+    vae_loss, recon_loss, kl_loss = vae_loss_function(x, x_r, mu, log_var, config)
+    
+    # Compute the loss for the classifier
+    clf_loss = classifier_loss(true_label, predicted_label)
+    
+    # Compute the final loss (note that the multiplication with the hyperparameter for Reconstruction and kl loss is done inside the vae_loss_function)
+    final_loss = vae_loss + config['gamma'] * clf_loss
+
+    return final_loss, recon_loss, kl_loss, clf_loss
+
+
 
