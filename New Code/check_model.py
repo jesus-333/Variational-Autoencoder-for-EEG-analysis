fc33402e0882000e151cade7da94ae3a55d49d16
jesus-333 <albi25.zanca@gmail.com>
2023-07-13 05:21:49 +0200

    Function to check model creation

---
+++ New Code/check_model.py
@@ -0,0 +1,35 @@
+"""
+@author: Alberto Zancanaro (Jesus)
+@organization: University of Padua (Italy)
+
+Functions used to check the creation of the various models
+"""
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+import torch
+
+import config_model as cm
+import model as md
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+def check_vEEGNet():
+    """
+    Function to check the absence of breaking bug during the creation and the forward pass of vEEGNet
+    """
+
+    C = 22
+    T = 512
+    hidden_space = 16
+    type_encoder = 0
+    type_decoder = 0
+
+    config = cm.get_config_vEEGNet(C, T, hidden_space, type_encoder, type_decoder)
+    model = md.vEEGNet(config)
+
+    x = torch.rand(5, 1, C, T)
+    x_r, z_mean, z_log_var = model(x)
+
+    print("Input shape : ", x.shape)
+    print("Output shape: ", x_r.shape)
+    print(z_mean.shape)
+    print(z_log_var.shape)
+
