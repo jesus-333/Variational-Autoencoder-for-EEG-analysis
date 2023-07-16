b76c710e965c4e324bb35f1d9e4276bc69d920fc
jesus-333 <albi25.zanca@gmail.com>
2023-07-16 19:21:45 +0200

    Work on hVAE

--- library/model/hierarchical_VAE.py
+++ library/model/hierarchical_VAE.py
@@ -12,17 +12,21 @@ Implementation of the (deep) Hierchical VAE
 import torch
 from torch import nn
 
-from . import support_function
+from . import support_function as sf
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 class hVAE(nn.Module):
 
-    def __init__(self, encoder_cell_list : list, decoder_cell_list : list):
+    def __init__(self, encoder_cell_list : list, decoder_cell_list : list, config : dict):
         super().__init__()
+        # Create encoder
+        self.encoder = hvae_encoder(encoder_cell_list, config)
+        _, _, encoder_output_shape = self.encoder.encode(torch.rand(1, 1, config['C'], config['T']))
+        config['encoder_output_shape'] = encoder_output_shape
 
-        self.encoder = nn.ModuleList(*encoder_cell_list)
-        self.decoder = nn.ModuleList(*decoder_cell_list)
+        # Create decoder
+        self.decoder = nn.ModuleList(*decoder_cell_list, config)
 
     def forward(self, x):
 
@@ -34,14 +38,14 @@ class hvae_encoder(nn.Module):
     def __init__(self, encoder_cell_list : list, config : dict):
         super().__init__()
 
-        self.encoder = nn.ModuleList(*encoder_cell_list)
+        self.encoder_modules = nn.ModuleList(*encoder_cell_list)
         
         # Compute the number of depth channels after the input flow through all the encoder
         tmp_x, _ = self.encode(torch.rand(1, 1, config['C'], config['T']))
         last_depth = tmp_x.shape[1]
         
         # Map the output of the last layer in the mean and logsigma of the distribution
-        self.map_to_distribution_parameters = map_to_distribution_parameters_with_convolution(last_depth, config['use_activation_last_layer'], config['activation'])
+        self.map_to_distribution_parameters = sf.map_to_distribution_parameters_with_convolution(last_depth, config['use_activation_last_layer'], config['activation'])
 
         self.convert_logvar_to_var = config['convert_logvar_to_var']
 
@@ -57,15 +61,20 @@ class hvae_encoder(nn.Module):
 
         return mu, sigma, cell_output
 
-    def encode(self, x):
+    def encode(self, x, return_shape = False):
         # List with the output of each cell of the encoder
         cell_output = []
+        output_shape = []
         
-        for cell in self.encoder: 
+        for cell in self.encoder_modules: 
             x = cell(x)
             cell_output.append(x)
-
-        return x, cell_output
+            if return_shape: output_shape.append(x.shape)
+        
+        if return_shape:
+            return x, cell_output, output_shape
+        else: 
+            return x, cell_output
 
 
 class hVAE_decoder(nn.Module):
@@ -73,44 +82,30 @@ class hVAE_decoder(nn.Module):
     def __init__(self, decoder_cell_list : list, config : dict):
         super().__init__()
         
-        # Temporary list to save section of the decoder
-        tmp_decoder = [] # Save the "main" module of the network
-        tmp_map_parameters = []
-        tmp_features_combination_z = [] # Save the modules used to combine the output of encoder and decoder
-        tmp_features_combination_decoder = [] # Save the modules used to combine the output of the decoder with the samples from the latens spaces
-
-        # Temporary input used to track shapes during network creations
-        tmp_x = torch.rand(config['shape_decoder_input'])
-        
-        for cell in decoder_cell_list:
-            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-            # Main network
-            tmp_decoder.append(cell)
-            tmp_x = cell(x)
-            tmp_map_parameters.append(map_to_distribution_parameters_with_convolution(tmp_x.shape[1], config['use_activation_in_decoder_distribution_map'], config['activation']))
-            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
-
+        # TODO
         self.decoder = nn.ModuleList(*tmp_decoder)
         self.map_to_distribution_parameters(*tmp_map_parameters)
         
         # Operation used to combine the various output of the encoder with the corresponding output of the decoder
         # Used only during training
         self.features_combination_z = nn.ModuleList(
-            *[weighted_sum_features_map() for i in range(len(decoder_cell_list) - 1)]
+            *[sf.weighted_sum_features_tensor() for i in range(len(decoder_cell_list) - 1)]
         )
         
         # Combine the output of the cell of the decoder with the z of the various latent spaces
         self.features_combination_decoder = nn.ModuleList(
-            *[weighted_sum_features_map() for i in range(len(decoder_cell_list))]
+            *[sf.weighted_sum_features_tensor() for i in range(len(decoder_cell_list))]
         )
 
 
     def forward(self, x, h = None, encoder_cell_output = None):
-        if h is None: h = torch.zeros(x.shape).to(x.device)
-        z = torch.cat([x, h], dim = 1)
-        x = self.features_combination_decoder[0](z)
+        if h is not None: 
+            z = torch.cat([x, h], dim = 1)
+        else:
+            z = x
 
         for i in range(len(self.decoder)):
+            x = self.features_combination_decoder[i](z)
             x = self.decoder[i](x)
             
             # Map the output of the decoder cell in mean (mu) and logvar (sigma)
@@ -122,34 +117,89 @@ class hVAE_decoder(nn.Module):
 
         return x 
             
+    def build_decoder(self, decoder_cell_list, config : dict):
+        # Temporary list to save section of the decoder
+        tmp_decoder = [] # Save the "main" module of the network
+        tmp_map_parameters = []
+        tmp_features_combination_z = [] # Save the modules used to combine the output of encoder and decoder that IT IS USED TO OBTAIN Z
+        tmp_features_combination_decoder = [] # Save the modules used to combine the output of the decoder with the samples from the latens spaces
 
-class weighted_sum_features_map(nn.Module):
-    def __init__(self, depth_x1 : int, depth_x2 : int, depth_output):
-        """
-        Implement a 1 x 1 convolution that mix togheter two tensors. The convolution works along the depth dimension.
-        Sinche the size of the kernel is (1 x 1) this operation is equal to a weighted sum between the features maps, with the weighted learned during the training
-        """
-        super().__init__()
+        # Temporary variables used to track shapes during network creations
+        tmp_x = torch.rand(config['encoder_output_shape'][-1])
+        tmp_z = torch.rand(config['encoder_output_shape'][-1])
+        
+        # List used to save the output of the decoder
+        encoder_output_shape = config['encoder_output_shape']
 
-        self.mix = nn.Conv2d(depth_x1 + depth_x2, depth_output, 1)
+        for i in range(len(decoder_cell_list)):
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
+            # Features combination latent space (i.e. Decoder/encoder output combination, merge to create the z)
+            # And paramters map
 
-    def forward(self, x1, x2):
-        x = torch.cat([x1, x2], dim = 1)
-        x = self.mix(x)
-        return x
+            if i > 0:
+                # Nota that in the deepest layer the output of the encoder IS the input of the encoder
+                # So there isn't necessity of combine anything
 
-class map_to_distribution_parameters_with_convolution(nn.Module):
-    def __init__(self, depth : int, use_activation : bool = False, activation : str = 'elu'):
-        """
-        Take in input a tensor of size B x D x H x W and return a tensor of size B x 2D x H x W
-        It practically doubles the size of the depth. The first D map will be used as mean and the other will be used a logvar 
-        """
-        super().__init__()
+                # Create and save the module
+                dec_enc_combination = sf.weighted_sum_tensor(encoder_output_shape[-1-i][1], tmp_x.shape[1], tmp_x.shape[1])
+                tmp_features_combination_z.append(dec_enc_combination)
+                
+                # Notes on the -1-i index. The output of the encoder are saved in the order obtained from the encoder
+                # So at position 0 we will have the output of the first cell of the encoder and at position -1 (the last) we will have the last output of the encoder
+                # For the DECODER we need to use this information in reverse order, i.e. from last to first. 
 
-        self.map = nn.Sequential(
-            support_function.get_activation(activation) if use_activation else nn.Identity(),
-            nn.Conv2d(depth, int(depth * 2), 1)
-        )
+                # Get the parameters map
+                if config['paramters_map_type'] == 0: # Convolution (i.e. matrix latent space)
+                    paramters_map = sf.map_to_distribution_parameters_with_convolution(depth = tmp_x.shape[1],
+                                                                                       use_activation = config['use_activation_in_decoder_distribution_map'], 
+                                                                                       activation = config['activation'])
+                elif config['paramters_map_type'] == 1: # Feedforward (i.e. vector latent space)
+                    paramters_map = sf.map_to_distribution_parameters_with_vector(hidden_space_dimension = config['hidden_space_dimension_list'][i],
+                                                                                  input_shape = tmp_x.shape,
+                                                                                  use_activation = config['use_activation_in_decoder_distribution_map'], 
+                                                                                  activation = config['activation'])
+                else:
+                    raise ValueError("config['paramters_map_type'] must have value 0 (convolution-matrix) or 1 (Feedforward-vector)")
 
-    def forward(self, x):
-        return self.map(x)
+                # Save the parameters map
+                tmp_map_parameters.append(paramters_map)
+
+                # Pass the "data" through the module
+                tmp_encoder_output = torch.rand(encoder_output_shape[-1-i])
+                tmp_z = paramters_map(tmp_x, tmp_encoder_output)
+
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+            # Features combination decoder
+            # z/decoder combination (i.e. after sampling from latent space combine the result with the output of the decoder)
+            
+            # Create the module to combine features
+            if i == 0:
+                if config['use_h_in_decoder']: # See Fig. 2 in NVAE paper
+                    # Module creation
+                    z_dec_combination = sf.weighted_sum_tensor(tmp_x.shape[1], config['h_shape'][1], tmp_x.shape[1])
+                    # Pass the "data"
+                    tmp_x = z_dec_combination(tmp_x, torch.rand(config['h_shape']))
+                else:
+                    z_dec_combination = nn.Identity()
+            else:
+                # Module creation
+                z_dec_combination = sf.weighted_sum_tensor(tmp_x.shape[1], tmp_z.shape[1], tmp_x.shape[1])
+            
+            # Save the module
+            tmp_features_combination_decoder.append(z_dec_combination)
+            
+            # Pass the "data" inside the module
+            tmp_x = z_dec_combination(tmp_x)
+
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+            # Main network 
+
+            # Get cell and obtain the output of the cell
+            cell = decoder_cell_list[i]
+            tmp_decoder.append(cell)
+            
+            # Pass the "data" through the cell
+            tmp_x = cell(tmp_x)
+
+        return tmp_decoder, tmp_map_parameters, tmp_features_combination_z, tmp_features_combination_decoder
+
--- library/model/support_function.py
+++ library/model/support_function.py
@@ -8,9 +8,11 @@ Minor support function used in the various script
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #%% Imports
 
+import torch
 from torch import nn
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Support during creation
 
 def get_activation(activation_name: dict):
     """
@@ -38,6 +40,119 @@ def get_dropout(prob_dropout: float, use_droput_2d : bool):
 
 
 def count_trainable_parameters(layer):
-    n_paramters = sum(p.numel() for p in  layer.parameters() if p.requires_grad)
-    return n_paramters
+    n_parameters = sum(p.numel() for p in  layer.parameters() if p.requires_grad)
+    return n_parameters
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# Pytorch Modules
+
+class weighted_sum_tensor(nn.Module):
+    def __init__(self, depth_x1 : int, depth_x2 : int, depth_output):
+        """
+        Implement a 1 x 1 convolution that mix togheter two tensors. The convolution works along the depth dimension.
+        Sinche the size of the kernel is (1 x 1) this operation is equal to a weighted sum between the features maps, with the weighted learned during the training
+        """
+        super().__init__()
+
+        self.mix = nn.Conv2d(depth_x1 + depth_x2, depth_output, 1)
+
+    def forward(self, x1, x2):
+        x = torch.cat([x1, x2], dim = 1)
+        x = self.mix(x)
+        return x
+
+class map_to_distribution_parameters_with_convolution(nn.Module):
+    def __init__(self, depth : int, use_activation : bool = False, activation : str = 'elu'):
+        """
+        Take in input a tensor of size B x D x H x W and return a tensor of size B x 2D x H x W
+        It practically doubles the size of the depth. The first D map will be used as mean and the other will be used a logvar 
+        """
+        super().__init__()
+
+        self.map = nn.Sequential(
+            get_activation(activation) if use_activation else nn.Identity(),
+            nn.Conv2d(depth, int(depth * 2), 1)
+        )
+
+    def forward(self, x):
+        x = self.map(x)
+        mean, log_var = x.chunk(2, 1)
+        return mean, log_var
+
+
+class map_to_distribution_parameters_with_vector(nn.Module):
+    def __init__(self, hidden_space_dimension : int, input_shape, use_activation : bool = False, activation : str = 'elu'):
+        """
+        Take in input a tensor of size B x D x H x W and return two vector of size hidden_space_dimension
+        The first vector will be used as mean and the second as log var
+        """
+        super().__init__()
+
+        n_neurons_input = input_shape[1] * input_shape[2] * input_shape[3]
+
+        self.map_mu = nn.Sequential(
+            get_activation(activation) if use_activation else nn.Identity(),
+            nn.Linear(n_neurons_input, hidden_space_dimension)
+        )
+
+        self.map_sigma = nn.Sequential(
+            get_activation(activation) if use_activation else nn.Identity(),
+            nn.Linear(n_neurons_input, hidden_space_dimension)
+        )
+
+    def forward(self, x):
+        return self.map_mu(x), self.map_sigma(x)
+
+
+class sample_layer(nn.Module):
+    def __init__(self, input_shape, config : dict, hidden_space_dimension : int = -1):
+        super().__init__()
+        """
+        PyTorch module used to implement the reparametrization trick and the sampling from the laten space in the hierarchical VAE (hVAE)
+        input_shape = shape of the input tensor
+        config = dictionary with some parameters
+        hidden_space_dimension = int with the dimension of the laten/hidden space. Used only if we have a vector as hidden variable
+        """
+
+        self.parameters_map_type = config['parameters_map_type']
+        self.input_shappe = input_shape
+
+        if self.parameters_map_type == 0: # Convolution (i.e. matrix latent space)
+            self.parameters_map = map_to_distribution_parameters_with_convolution(depth = input_shape[1],
+                                                                               use_activation = config['use_activation_in_decoder_distribution_map'], 
+                                                                               activation = config['activation'])
+        elif self.parameters_map_type == 1: # Feedforward (i.e. vector latent space)
+            self.parameters_map = map_to_distribution_parameters_with_vector(hidden_space_dimension = hidden_space_dimension,
+                                                                          input_shape = input_shape,
+                                                                          use_activation = config['use_activation_in_decoder_distribution_map'], 
+                                                                          activation = config['activation'])
+            n_neurons_input = hidden_space_dimension
+            n_neurons_output = input_shape[1] * input_shape[2] * input_shape[3]
+            
+            self.ff_layer = nn.Sequential(
+                nn.Linear(n_neurons_input, n_neurons_output),
+                get_activation(config['activation']) if config['use_activation'] else nn.Identity(),
+            )
+
+        else:
+            raise ValueError("config['parameters_map_type'] must have value 0 (convolution-matrix) or 1 (Feedforward-vector)")
+
+    def forward(self, x):
+        if self.parameters_map_type == 1: # Feedforward
+            x = x.flatten(1)
+
+        mean, log_var = self.parameters_map(x)
+
+        z = self.reparametrize(mean, log_var)
+
+        if self.parameters_map_type == 0: # Convolution
+
+        elif self.parameters_map_type == 1: # Feedforward (i.e. vector latent space)
+            x = z
+        else:
+            raise ValueError("config['parameters_map_type'] must have value 0 (convolution-matrix) or 1 (Feedforward-vector)")
+
+        return x
+
+    def reparametrize(self, mean, log_var):
+        var = torch.exp(0.5 * log_var)
