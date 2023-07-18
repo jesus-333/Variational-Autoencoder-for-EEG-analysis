8a682bb94c3e375107cd177365ce017b80c4969a
jesus-333 <albi25.zanca@gmail.com>
2023-07-18 05:41:44 +0200

    Work on hvEEGNet training

--- library/loss_function.py
+++ library/loss_function.py
@@ -30,13 +30,26 @@ def recon_loss_frequency_function(x, x_r):
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # Kullback loss
 
-def kl_loss_normal_function(mu, log_var):
+def kl_loss_normal_function(mu, log_var, delta_mu = None, delta_log_var = None):
     """
     Compute the Kullback–Leibler divergence between the distribution parametrized by mu and log var and a normal distribution
     """
-    return torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+
+    # If mu is a matrix it will have shape [B x D x H x W] so len(shape) = 4 > 2
+    # If mu is a vector it will have shape [B x N] so len(shape)
+    if len(mu.shape) > 2: dim_sum = [1,2,3]
+    else: dim_sum = 1
+
+    if delta_mu is None and delta_log_var is None: # Classic KL
+        return torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = dim_sum), dim = 0)
+    else: # "Shifted" KL (Section 3.2 NVAE Paper)
+        return -0.5 * torch.sum(1 + delta_log_var - delta_mu ** 2 / log_var.exp() - delta_log_var.exp(), dim = dim_sum)
+
 
 def kl_loss_function(mu_1, log_var_1, mu_2, log_var_2):
+    """
+    General form of the KL divergence between two gaussian distribution
+    """
     pass
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -69,4 +82,21 @@ def vEEGNet_loss(x, x_r, mu, log_var, true_label, predicted_label, config):
     return final_loss, recon_loss, kl_loss, clf_loss
 
 
+def hvEEGNet_loss(x, x_r, mu_list, log_var_list, delta_mu_list, delta_log_var_list, config : dict):
+    recon_loss = recon_loss_function(x, x_r)
+    
+    kl_loss = 0
+    for i in range(len(mu_list)):
+        if i == 0:
+            tmp_kl = kl_loss_normal_function(mu_list[i], log_var_list[i])
+        else:
+            tmp_kl = kl_loss_normal_function(mu_list[i], log_var_list[i], delta_mu_list[i - 1], delta_log_var_list[i - 1])
+            # Remember that there are 1 element less the of the shift term (i.e. delta_mu and delta_log_var) respect the mus and log_vars
+            # This because the deepest layer has not a shift term
+
+        kl += tmp_kl
+
+    final_loss = config['alpha'] * recon_loss + config['beta'] * kl_loss
+
+    return final_loss, recon_loss, kl_loss
 
---
+++ library/train_hvEEGNet.py
@@ -0,0 +1,93 @@
+"""
+@author: Alberto Zancanaro (Jesus)
+@organization: University of Padua (Italy)
+
+Train function of the hierarchical vEEGnet
+"""
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#%% Imports
+
+# Python library
+import torch
+
+# Config files
+from .config import config_model as cm
+from .config import config_dataset as cd
+from .config import config_training as ct
+
+import train_generic
+    
+"""
+%load_ext autoreload
+%autoreload 2
+
+import sys
+"""
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#%% Epochs function
+
+def train_epoch(model, loss_function, optimizer, train_loader, train_config):
+    # Set the model in training mode
+    model.train()
+
+    # Variable to accumulate the loss
+    train_loss = 0
+
+    for sample_data_batch, sample_label_batch in train_loader:
+        # Move data to training device
+        x = sample_data_batch.to(train_config['device'])
+        true_label = sample_label_batch.to(train_config['device'])
+
+        # Zeros past gradients
+        optimizer.zero_grad()
+        
+        # Networks forward pass
+        x_r, mu_list, log_var_list, delta_mu_list, delta_log_var_list = model(x)
+        
+        # Loss evaluation
+        batch_train_loss = loss_function(x, x_r, mu_list, log_var_list, delta_mu_list, delta_log_var_list)
+    
+        # Backward/Optimization pass
+        batch_train_loss.backward()
+        optimizer.step()
+
+        # Accumulate the loss
+        train_loss += batch_train_loss * x.shape[0]
+
+    # Compute final loss
+    train_loss = train_loss / len(train_loader.sampler)
+    
+    return train_loss
+
+
+def validation_epoch(model, loss_function, validation_loader, train_config):
+    # Set the model in evaluation mode
+    model.eval()
+
+    # Variable to accumulate the loss
+    validation_loss = 0
+
+    for sample_data_batch, sample_label_batch in validation_loader:
+        # Move data to training device
+        x = sample_data_batch.to(train_config['device'])
+        true_label = sample_label_batch.to(train_config['device'])
+
+        # Disable gradient tracking
+        with torch.no_grad():
+            # Forward pass
+            x_r, mu_list, log_var_list, delta_mu_list, delta_log_var_list = model(x)
+            
+            # Loss evaluation
+            batch_validation_loss = loss_function(x, x_r, mu_list, log_var_list, delta_mu_list, delta_log_var_list)
+            
+            # Accumulate loss
+            validation_loss += batch_validation_loss * x.shape[0]
+
+    # Compute final loss
+    validation_loss = validation_loss / len(validation_loader.sampler)
+    
+    return validation_loss
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
