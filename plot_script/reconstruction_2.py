7d7cbb18de66ebc47dd403e6c77262d3fe37adb3
jesus-333 <albi25.zanca@gmail.com>
2023-08-29 16:13:32 +0200

    Upload reconstruction_2.py

--- plot_script/reconstruction_2.py
+++ plot_script/reconstruction_2.py
@@ -29,6 +29,9 @@ loss = 'dtw'
 epoch_list = [20, 40, 'BEST']
 epoch_list = [40]
 
+use_test_set = True
+use_cuda = True if device == 'cuda' else False
+
 def get_dataset_and_model(subj_list):
     dataset_config = cd.get_moabb_dataset_config(subj_list)
     device = 'cpu'
@@ -48,12 +51,46 @@ def get_dataset_and_model(subj_list):
 
     return train_dataset, validation_dataset, test_dataset , model_hv
 
-def compute_loss_dataset(dataset, model, path_weight):
-    model.load_state_dict(torch.load(path_weight, map_location = torch.device('cpu')))
+def compute_loss_dataset(dataset, model, use_cuda):
     recon_loss_function = SoftDTW(use_cuda = use_cuda, normalize = False)
+
+    recon_loss_matrix = np.zeros((len(dataset), dataset.ch_list))
     
-    for i in range(len(dataset)):
-        pass
+    with torch.no_grad():
+        for i in range(len(dataset)):
+            # Get the original signal and reconstruct it
+            x = dataset[i].unsqueeze(0) # Add batch dimension
+            x_r = model.reconstruct(x)
+            
+            # Compute the DTW channel by channels
+            tmp_recon_loss = []
+            for i in range(x.shape[2]): # Iterate through EEG Channels
+                x_ch = x[:, :, i, :].swapaxes(1,2)
+                x_r_ch = x_r[:, :, i, :].swapaxes(1,2)
+                # Note that the depth dimension has size 1 for EEG signal. So after selecting the channel x_ch will have size [B x D x T], with D = depth = 1
+                # The sdtw want the length of the sequence in the dimension with the index 1 so I swap the depth dimension and the the T dimension
+                
+                tmp_recon_loss.append(float(recon_loss_function(x_ch, x_r_ch)))
+
+            recon_loss_matrix[i, :] = tmp_recon_loss
+
+    return recon_loss_matrix
 
 #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+recon_loss_results = dict()
+
+for subj in subj_list:
+    recon_loss_results[subj] = dict()
+
+    for epoch in epoch_list:
+        train_dataset, validation_dataset, test_dataset , model_hv = get_dataset_and_model([subj])
+
+        if use_test_set: dataset = test_dataset
+        else: dataset = train_dataset
+        
+        # Load model weight
+        path_weight = 'Saved Model/hvEEGNet_shallow_{}/{}/model_{}.pth'.format(loss, subj, epoch)
+        model_hv.load_state_dict(torch.load(path_weight, map_location = torch.device('cpu')))
+
+        recon_loss_results[subj][epoch] = compute_loss_dataset(dataset, model_hv, path_weight)
