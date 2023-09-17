baee7faffa48acc24f13406c946c55a128e09275
jesus-333 <albi25.zanca@gmail.com>
2023-09-17 16:26:40 +0200

    Update reconstruction_3.py

--- analysisi_script/reconstruction_3.py
+++ analysisi_script/reconstruction_3.py
@@ -32,12 +32,12 @@ if len(sys.argv) > 1:
     subj_list = sys.argv[2]
 else:
     tot_epoch_training = 80
-    subj_list = [2, 9]
+    subj_list = [2]
     repetition_list = np.arange(19) + 1
     epoch_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
     use_test_set = False
 
-batch_size = 64
+batch_size = 144
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
@@ -65,10 +65,12 @@ for subj in subj_list:
             path_weight = 'Saved Model/repetition_hvEEGNet_{}/subj {}/rep {}/model_{}.pth'.format(tot_epoch_training, subj, repetition, epoch)
             model_hv.load_state_dict(torch.load(path_weight, map_location = torch.device('cpu')))
             
+            tmp_recon_loss = support.compute_loss_dataset(dataset, model_hv, device, batch_size) / 1000
+            
             if epoch not in recon_loss_results[subj]:    
-                recon_loss_results[subj][epoch] = support.compute_loss_dataset(dataset, model_hv, device, batch_size) / 1000
+                recon_loss_results[subj][epoch] = tmp_recon_loss
             else:
-                recon_loss_results[subj][epoch] += support.compute_loss_dataset(dataset, model_hv, device, batch_size) / 1000
+                recon_loss_results[subj][epoch] += tmp_recon_loss
               
             # Save the results for each repetition
             path_save = 'Saved Results/repetition_hvEEGNet_{}/subj {}/'.format(tot_epoch_training, subj)
@@ -76,11 +78,11 @@ for subj in subj_list:
             
             path_save = 'Saved Results/repetition_hvEEGNet_{}/subj {}/recon_error_{}_rep_{}.pickle'.format(tot_epoch_training, subj, epoch, repetition)
             pickle_out = open(path_save, "wb") 
-            pickle.dump(recon_loss_results[subj][epoch] , pickle_out) 
+            pickle.dump(tmp_recon_loss , pickle_out) 
             pickle_out.close() 
             
             path_save = 'Saved Results/repetition_hvEEGNet_{}/subj {}/recon_error_{}_rep_{}.npy'.format(tot_epoch_training, subj, epoch, repetition)
-            np.save(path_save, recon_loss_results[subj][epoch])
+            np.save(path_save, tmp_recon_loss)
 
 #%% Average accross repetition and save the results
 for subj in subj_list:
