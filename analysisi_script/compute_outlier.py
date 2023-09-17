6c50680050b623483aa2371b0739fe5a6a2c1045
jesus-333 <albi25.zanca@gmail.com>
2023-09-17 22:01:59 +0200

    Work on outlier detection (reconstruction error)

--- analysisi_script/compute_outlier.py
+++ analysisi_script/compute_outlier.py
@@ -16,58 +16,71 @@ from library.config import config_dataset as cd
 #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 # Parameters
 
-subj = 2
+tot_epoch_training = 80
+subj_list = [2, 9]
+epoch = 80
+
 neighborhood_order = 5
+knn_algorithm = 'auto'
+s_knee = 1
 
-knn_algorithm = 'ball_tree'
-
-path_recon_error = ''
-
-#%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Load data
-
-dataset_config = cd.get_moabb_dataset_config([subj])
-dataset_config['percentage_split_train_validation'] = -1
-train_dataset, validation_dataset, test_dataset , model_hv = support.get_dataset_and_model(dataset_config)
-
-# Load the reconstruction error
-recon_error = np.load(path_recon_error)
+plot_config = dict(
+    figsize = (12, 8),
+    fontsize = 12,
+)
 
 #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-# Outlier identifications
 
-# Compute the KNN
-neighborhood_set   = knn(n_neighbors = neighborhood_order, algorithm = knn_algorithm).fit(recon_error)
-distances, indices = neighborhood_set.kneighbors(recon_error)
+plt.rcParams.update({'font.size': plot_config['fontsize']})
+fig, ax = plt.subplots(1, 1, figsize = plot_config['figsize'])
 
-# compute distances from nth nearest neighbors (given by neighborhood_order) and sort them
-dk_sorted     = np.sort(distances[:,-1])
-dk_sorted_ind = np.argsort(distances[:,-1])
+for subj in subj_list:
+    path_recon_error = './Saved Results/repetition_hvEEGNet_{}/subj {}/recon_error_{}_average.npy'.format(tot_epoch_training, subj, epoch)
+    # Load data
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-#%% Knee plot
+    # dataset_config = cd.get_moabb_dataset_config([subj])
+    # dataset_config['percentage_split_train_validation'] = -1
+    # train_dataset, validation_dataset, test_dataset , model_hv = support.get_dataset_and_model(dataset_config)
 
-try:
-    from kneed import KneeLocator
-    i = np.arange(len(distances))
-    knee = KneeLocator(i, dk_sorted, S = 1, curve = 'convex', direction = 'increasing', interp_method = 'interp1d', online = True)
-    knee_x = knee.knee
-    knee_y = knee.knee_y    # OR: distances[knee.knee]
+    # Load the reconstruction error
+    recon_error = np.load(path_recon_error)
 
-    print([knee_x, np.round(knee_y,2)])
-    # Plot distances
-    fig = plt.figure(figsize=(18,2))
+    #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    # Outlier identifications
 
-    ax = fig.add_subplot(111)
-    plt.plot(dk_sorted, 'o-')
-    ax.set_xlabel('Data points', fontsize=10)
-    ax.set_ylabel('Distances (sorted)', fontsize=10)
-    plt.axvline(x = knee_x, color='k', linestyle='--')
-    plt.axhline(y = knee_y, color='k', linestyle='--')
-    plt.plot((knee_x), (knee_y), 'o', color='r')
-    plt.grid()
-    plt.show()
+    # Compute the KNN
+    neighborhood_set   = knn(n_neighbors = neighborhood_order, algorithm = knn_algorithm).fit(recon_error)
+    distances, indices = neighborhood_set.kneighbors(recon_error)
 
-except ImportError as e:
-    print("Error -> ", e)
-    print("If you want the knee plot install the kneed package (pip install kneed)")
+    # compute distances from nth nearest neighbors (given by neighborhood_order) and sort them
+    dk_sorted     = np.sort(distances[:,-1])
+    dk_sorted_ind = np.argsort(distances[:,-1])
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    #%% Knee plot
+
+    try:
+        from kneed import KneeLocator
+        i = np.arange(len(distances))
+        knee = KneeLocator(i, dk_sorted, S = s_knee, curve = 'convex', direction = 'increasing', interp_method = 'interp1d', online = True)
+        knee_x = knee.knee
+        knee_y = knee.knee_y    # OR: distances[knee.knee]
+
+        print("Number of outliers for subj {} (S = {}): {}".format(subj, s_knee, recon_error.shape[0] - knee_x))
+
+        ax.plot(dk_sorted, 'o-', label = 'Subject {}'.format(subj))
+        ax.set_xlabel('EEG Trials', fontsize = plot_config['fontsize'])
+        ax.set_ylabel('Distances (sorted)', fontsize = plot_config['fontsize'])
+        ax.axvline(x = knee_x, color = 'k', linestyle = '--')
+        ax.axhline(y = knee_y, color = 'k', linestyle = '--')
+        ax.plot((knee_x), (knee_y), 'o', color = 'r')
+        ax.set_title("Knee plot at epoch {}".format(epoch))
+        ax.grid()
+        ax.legend()
+
+        fig.tight_layout()
+        fig.show()
+
+    except ImportError as e:
+        print("Error -> ", e)
+        print("If you want the knee plot install the kneed package (pip install kneed)")
