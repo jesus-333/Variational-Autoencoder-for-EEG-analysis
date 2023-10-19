0f5ecda49a41d8e30e7cdb7a5c3c059fcc97301d
jesus-333 <albi25.zanca@gmail.com>
2023-10-19 17:42:42 +0200

    Update reconstruction_3.py

--- analysisi_script/reconstruction_3.py
+++ analysisi_script/reconstruction_3.py
@@ -25,14 +25,14 @@ if len(sys.argv) > 1:
     subj = sys.argv[2]
 else:
     tot_epoch_training = 80
-    subj = 9
+    subj = 6
 
 repetition_list = np.arange(20) + 1
 epoch_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
-epoch_list = [80]
+# epoch_list = [80]
 use_test_set = True
 
-batch_size = 72
+batch_size = 96
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -52,9 +52,10 @@ train_dataset, validation_dataset, test_dataset , model_hv = support.get_dataset
 
 for repetition in repetition_list:
     print("\tRep: ", repetition)
-    if subj == 4 and (repetition == 4 or repetition == 6): continue
-    if subj == 5 and repetition == 19: continue
-    if subj == 8 and repetition == 12: continue
+    if use_test_set == False:
+        if subj == 4 and (repetition == 4 or repetition == 6): continue
+        if subj == 5 and repetition == 19: continue
+        if subj == 8 and repetition == 12: continue
     
     for epoch in epoch_list:
         if use_test_set: 
@@ -90,9 +91,9 @@ for repetition in repetition_list:
         os.makedirs(path_save, exist_ok = True)
 
         path_save = 'Saved Results/repetition_hvEEGNet_{}/{}/subj {}/recon_error_{}_rep_{}.pickle'.format(tot_epoch_training, string_dataset, subj, epoch, repetition)
-        pickle_out = open(path_save, "wb")
-        pickle.dump(tmp_recon_loss , pickle_out)
-        pickle_out.close()
+        # pickle_out = open(path_save, "wb")
+        # pickle.dump(tmp_recon_loss , pickle_out)
+        # pickle_out.close()
 
         path_save = 'Saved Results/repetition_hvEEGNet_{}/{}/subj {}/recon_error_{}_rep_{}.npy'.format(tot_epoch_training, string_dataset, subj, epoch, repetition)
         np.save(path_save, tmp_recon_loss)
@@ -104,9 +105,9 @@ for epoch in epoch_list:
     os.makedirs(path_save, exist_ok = True)
 
     path_save = 'Saved Results/repetition_hvEEGNet_{}/{}/subj {}/recon_error_{}_average.pickle'.format(tot_epoch_training, string_dataset, subj, epoch)
-    pickle_out = open(path_save, "wb")
-    pickle.dump(recon_loss_results[subj][epoch] / valid_repetition_per_epoch[epoch] , pickle_out)
-    pickle_out.close()
+    # pickle_out = open(path_save, "wb")
+    # pickle.dump(recon_loss_results[subj][epoch] / valid_repetition_per_epoch[epoch] , pickle_out)
+    # pickle_out.close()
 
     path_save = 'Saved Results/repetition_hvEEGNet_{}/{}/subj {}/recon_error_{}_average.npy'.format(tot_epoch_training, string_dataset, subj, epoch)
     np.save(path_save, recon_loss_results[subj][epoch] / valid_repetition_per_epoch[epoch])
