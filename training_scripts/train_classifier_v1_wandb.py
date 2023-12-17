c5661f439b7707bac9d50e6e2b491754663a8855
jesus-333 <albi25.zanca@gmail.com>
2023-12-17 23:05:18 +0100

    Update train_classifier_v1_wandb.py
    Improve notes for wandb and variable names

--- training_scripts/train_classifier_v1_wandb.py
+++ training_scripts/train_classifier_v1_wandb.py
@@ -11,12 +11,13 @@ from library.config import config_training as ct
 
 subj_data = 2
 subj_weights = 1
+repetition_list = [1]
 
-repetition = 1
 epoch_trained = 80
+hvEEGNet_training_repetition = 1 # Since for each subject we train hvEEGNet 20 times this parameter specifies the training run
 
-path_weights_hvEEGNet = 'Saved Model/repetition_hvEEGNet_80/subj {}/rep {}/model_{}.pth'.format(subj_weights, repetition, epoch_trained)
-
+path_weights_hvEEGNet = 'Saved Model/repetition_hvEEGNet_80/subj {}/rep {}/model_{}.pth'.format(subj_weights, hvEEGNet_training_repetition, epoch_trained)
+    
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
 dataset_config, _, hvEEGNet_config = wt.get_config_dict_for_hvEEGNet_shallow([subj_data])
@@ -32,18 +33,23 @@ model_config = dict(
 train_config = ct.get_config_classifier()
 train_config['wandb_training'] = True
 train_config['project_name'] = 'hvEEGNet_classifier'
-train_config['model_artifact_name'] = 'classifier'
+train_config['model_artifact_name'] = 'classifier_v1'
 
-train_config['extra_info_training'] = dict(
-    subj_data = subj_data,
-    subj_weights = subj_weights,
-    repetition = repetition,
-    epoch_trained = epoch_trained,
-    path_weights_hvEEGNet = path_weights_hvEEGNet
-)
+for repetition in repetition_list:
 
-train_config['epochs'] = 3 
-train_config['device'] = 'cpu'
-
-model_name = 'classifier_v1'
-model = wt.train_wandb(model_name, dataset_config, train_config, model_config)
+    train_config['extra_info_training'] = dict(
+        subj_data = subj_data,
+        subj_weights = subj_weights,
+        hvEEGNet_training_repetition = hvEEGNet_training_repetition,
+        hvEEGNet_epoch_trained = epoch_trained,
+        path_weights_hvEEGNet = path_weights_hvEEGNet
+    )
+    
+    train_config['notes'] = "classifier_v1_Weights_S{}_Target_S{}_rep_{}".format(subj_weights, subj_data, repetition)
+    
+    train_config['name'] = ""
+    train_config['name'] += "Training of classifier v1 with the weights of S{} and the data of S{}\n".format(subj_weights, subj_data)
+    train_config['name'] += "The weights are from hvEEGNet at the end of the training with {} epochs (repetition {})".format(epoch_trained, hvEEGNet_training_repetition)
+    
+    model_name = 'classifier_v1'
+    model = wt.train_wandb(model_name, dataset_config, train_config, model_config)
