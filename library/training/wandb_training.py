558c53c6a80da4f38cbfabd2b6aa7b3cabe1ac74
Jesus <albi25.zanca@gmail.com>
2024-03-28 15:38:46 +0100

    Add new wandb trainig function
    Add another training function with wandb support that use the train() function instead of train_and_test()

--- library/training/wandb_training.py
+++ library/training/wandb_training.py
@@ -10,13 +10,14 @@ Script with the function to train the various network and save the results with 
 
 # Python library
 import wandb
+import torch
 
 # Custom functions
 from . import train_generic
 
 # Config files
 from ..config import config_model as cm
-from ..config import config_dataset as cd 
+from ..config import config_dataset as cd
 from ..config import config_training as ct
 
 """
@@ -28,7 +29,11 @@ import sys
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-def train_wandb(model_name, dataset_config, train_config, model_config):
+def train_wandb_V1(model_name : str, dataset_config : dict, train_config : dict, model_config : dict):
+    """
+    Train a model with the data downloaded through the moabb tools.
+    """
+
     notes = train_config['notes']
     name = train_config['name'] if 'name' in train_config else None
 
@@ -53,9 +58,66 @@ def train_wandb(model_name, dataset_config, train_config, model_config):
         run.log_artifact(model_artifact)
 
         return model
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-def main_EEGNet_classifier(): 
+
+def train_wandb_V2(model_name : str, train_config : dict, model_config : dict, optimizer, train_dataset, validation_dataset):
+    """
+    Train a model with the data provided by the user and log everything on wandb.
+
+    @param model_name: string with the name of the model.
+    @param train_config: dictionary with all the hyperparameter for the training. Check the README_training for more info.
+    @param model_config: dictionary with all the parameter to use in the creation of the model. Check README_model for more info.
+    @param optimizer: PyTorch optimizer to use during the training (e.g. AdamW)
+    @param loader_list: list with two dataloader. The first dataloader is for the training data and the second for the test data.
+    @return: the trained model.
+    """
+
+    notes = train_config['notes'] if 'notes' in train_config else 'No notes in train_config'
+    name = train_config['name'] if 'name' in train_config else None
+
+    wandb_config = dict(
+        train = train_config,
+        model = model_config
+    )
+    
+    # Create dataloader
+    train_dataloader        = torch.utils.data.DataLoader(train_dataset, batch_size = train_config['batch_size'], shuffle = True)
+    validation_dataloader   = torch.utils.data.DataLoader(validation_dataset, batch_size = train_config['batch_size'], shuffle = True)
+    loader_list             = [train_dataloader, validation_dataloader]
+
+    # Create model
+    model_config['input_size'] = train_dataset[0][0].unsqueeze(0).shape
+    model = train_generic.get_untrained_model(model_name, model_config)
+    model.to(train_config['device'])
+
+    # Declare loss function
+    loss_function = train_generic.get_loss_function(model_name, train_config)
+
+    # Setup lr scheduler
+    if train_config['use_scheduler'] :
+        lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = train_config['lr_decay_rate'])
+    else:
+        lr_scheduler = None
+
+    with wandb.init(project = train_config['project_name'], job_type = "train", config = wandb_config, notes = notes, name = name) as run:
+        # Setup artifact to save model
+        model_artifact_name = train_config['model_artifact_name'] + '_trained'
+        metadata = dict(training_config = dict(train_config))
+        model_artifact = wandb.Artifact(model_artifact_name, type = "model",
+                                        description = "Trained {} model".format(train_config['model_artifact_name']),
+                                        metadata = metadata)
+        
+        # Train the model
+        model = train_generic.train(model, loss_function, optimizer, loader_list, train_config, lr_scheduler, model_artifact)
+        
+        # Log the model artifact
+        run.log_artifact(model_artifact)
+
+        return model
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+def main_EEGNet_classifier():
     dataset_config = cd.get_moabb_dataset_config([3])
     
     train_config = ct.get_config_classifier()
@@ -65,11 +127,11 @@ def main_EEGNet_classifier():
     T = 512 
     model_config = cm.get_config_EEGNet_stft_classifier(C, T, 22)
     
-    model = train_wandb('EEGNet', dataset_config, train_config, model_config)
+    model = train_wandb_V1('EEGNet', dataset_config, train_config, model_config)
     
     return model
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def get_config_dict_for_vEEGNet(subj_list : list):
     dataset_config = cd.get_moabb_dataset_config(subj_list)
@@ -81,7 +143,7 @@ def get_config_dict_for_vEEGNet(subj_list : list):
     C = 22
     if dataset_config['resample_data']: sf = dataset_config['resample_freq']
     else: sf = 250
-    T = int((dataset_config['trial_end'] - dataset_config['trial_start']) * sf )
+    T = int((dataset_config['trial_end'] - dataset_config['trial_start']) * sf)
     hidden_space_dimension = 64
     type_encoder = 0
     type_decoder = 0
@@ -105,11 +167,11 @@ def main_vEEGNet(dataset_config : dict, train_config : dict, model_config):
     (This method is also used in colab for training instead of main_vEEGNet_default because in colab it is not possible to modify other files)
     """
 
-    model = train_wandb('vEEGNet', dataset_config, train_config, model_config)
+    model = train_wandb_V1('vEEGNet', dataset_config, train_config, model_config)
     
     return model
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 def get_config_dict_for_hvEEGNet_shallow(subj_list : list):
     dataset_config = cd.get_moabb_dataset_config(subj_list)
@@ -121,7 +183,7 @@ def get_config_dict_for_hvEEGNet_shallow(subj_list : list):
     C = 22
     if dataset_config['resample_data']: sf = dataset_config['resample_freq']
     else: sf = 250
-    T = int((dataset_config['trial_end'] - dataset_config['trial_start']) * sf )
+    T = int((dataset_config['trial_end'] - dataset_config['trial_start']) * sf)
     type_decoder = 0
     parameters_map_type = 0
     model_config = cm.get_config_hierarchical_vEEGNet(C, T, type_decoder, parameters_map_type)
@@ -133,7 +195,7 @@ def get_config_dict_for_hvEEGNet_shallow(subj_list : list):
 
 def main_hvEEGNet_shallow(dataset_config, train_config, model_config):
 
-    model = train_wandb('hvEEGNet_shallow', dataset_config, train_config, model_config)
+    model = train_wandb_V1('hvEEGNet_shallow', dataset_config, train_config, model_config)
     
     return model
 
