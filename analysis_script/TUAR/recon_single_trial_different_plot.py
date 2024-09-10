5f0e10b7177d249de2e70a4dd3abd4745119581e
jesus-333 <albi25.zanca@gmail.com>
2024-09-10 16:50:07 +0200

    Minor update

--- analysis_script/TUAR/recon_single_trial_different_plot.py
+++ analysis_script/TUAR/recon_single_trial_different_plot.py
@@ -28,9 +28,10 @@ def low_pass_data(data, config : dict) :
 
 # N.b. RICORDATI NORMALIZATION DEI TRAILS
 filename = 'NO_NOTCH_train8'
+filename = 'NO_NOTCH_shuffle6'
 tot_epoch_training = 30
-epoch = 20
-use_test_set = False
+epoch = 30
+use_test_set = True
 
 t_min = 0
 t_max = 4
@@ -40,29 +41,30 @@ nperseg = None
 
 # If rand_trial_sample == True the trial to plot are selected randomly below
 rand_trial_sample = True
-plot_to_create = 15
+plot_to_create = 20
 
-n_trial = 77
-channel = 12
+n_trial = 327
+channel = 10
 # channel = np.random(['Fp1', 'Fp2', 'FC3', 'FCz', 'FC4', 'C3', 'Cz', 'C4', 'CP3', 'CPz','CP4', 'O1', 'Oz', 'O2'])
 
 filter_config = dict(
     use_filter = False,
     fs = 250,
-    cutoff = 60,
+    cutoff = 80,
     order = 5
 )
 
 plot_config = dict(
-    figsize_time = (10, 5),
-    figsize_freq = (10, 5),
+    rescale_minmax = True,
+    figsize_time = (15, 8),
+    figsize_freq = (15, 8),
     fontsize = 18,
     linewidth_original = 1.5,
     linewidth_reconstructed = 1.5,
     color_original = 'black',
     color_reconstructed = 'red',
     add_title = False,
-    save_fig = False,
+    save_fig = True,
     # format_so_save = ['png', 'pdf', 'eps']
     format_so_save = ['png']
 )
@@ -76,7 +78,6 @@ path_dataset_config = 'training_scripts/config/TUAR/dataset.toml'
 path_model_config = 'training_scripts/config/TUAR/model.toml'
 path_traing_config = 'training_scripts/config/TUAR/training.toml'
 
-
 #%% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Dataset and model creation
 
@@ -84,8 +85,8 @@ if rand_trial_sample == False : plot_to_create = 1
 plt.rcParams.update({'font.size': plot_config['fontsize']})
 
 # Load data
-data_train = np.load('data/TUAR/NO_NOTCH_train8.npz')['train_data']
-data_test = np.load('data/TUAR/NO_NOTCH_train8.npz')['test_data']
+data_train = np.load('data/TUAR/{}.npz'.format(filename))['train_data']
+data_test = np.load('data/TUAR/{}.npz'.format(filename))['test_data']
 
 # Create train and validation dataset
 dataset_train = ds_time.EEG_Dataset(data_train, np.ones(len(data_train)), ch_list = [])
@@ -106,6 +107,7 @@ model_config['encoder_config']['C'] = C
 model_config['encoder_config']['T'] = T
 model_config['encoder_config']['c_kernel_2'] = [C, 1]
 model_hv = train_generic.get_untrained_model('hvEEGNet_shallow', model_config)
+# model_hv.eval()
 
 # Decide if use the train or the test dataset
 if use_test_set : dataset = dataset_test
@@ -123,14 +125,14 @@ for n_plot in range(plot_to_create):
     
     # Get trial and create vector for time and channel
     x, _ = dataset[n_trial]
-    tmp_t = np.linspace(1, 5, x.shape[-1])
+    tmp_t = np.linspace(0, 4, x.shape[-1])
     idx_t = np.logical_and(tmp_t >= t_min, tmp_t <= t_max)
     t = tmp_t[idx_t]
     idx_ch = np.arange(data_train.shape[2]) == channel
     
     # Load weight and reconstruction
     path_weight = 'Saved Model/TUAR/{}_{}_epochs/model_{}.pth'.format(filename, tot_epoch_training, epoch) 
-    path_weight = 'Saved Model/repetition_hvEEGNet_80/subj 3/rep 13/model_80.pth'
+    # path_weight = 'Saved Model/repetition_hvEEGNet_80/subj 3/rep 13/model_80.pth'
     model_hv.load_state_dict(torch.load(path_weight, map_location = torch.device('cpu')))
     x_r = model_hv.reconstruct(x.unsqueeze(0)).squeeze()
 
@@ -162,8 +164,9 @@ for n_plot in range(plot_to_create):
     fig_time, ax_time = plt.subplots(1, 1, figsize = plot_config['figsize_time'])
     
     # TMP
-    x_original_to_plot = (x_original_to_plot - x_original_to_plot.min()) / (x_original_to_plot.max() - x_original_to_plot.min())
-    x_r_to_plot = (x_r_to_plot - x_r_to_plot.min()) / (x_r_to_plot.max() - x_r_to_plot.min())
+    if plot_config['rescale_minmax'] :
+        x_original_to_plot = (x_original_to_plot - x_original_to_plot.min()) / (x_original_to_plot.max() - x_original_to_plot.min())
+        x_r_to_plot = (x_r_to_plot - x_r_to_plot.min()) / (x_r_to_plot.max() - x_r_to_plot.min())
     
     ax_time.plot(t, x_original_to_plot, label = 'original signal',
                  color = plot_config['color_original'], linewidth = plot_config['linewidth_original'])
@@ -185,6 +188,10 @@ for n_plot in range(plot_to_create):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Plot in frequency domain
 
+    if plot_config['rescale_minmax'] :
+        x_psd = (x_psd - x_psd.min()) / (x_psd.max() - x_psd.min())
+        x_r_psd = (x_r_psd - x_r_psd.min()) / (x_r_psd.max() - x_r_psd.min())
+
     fig_freq, ax_freq = plt.subplots(1, 1, figsize = plot_config['figsize_freq'])
     ax_freq.plot(f, x_psd, label = 'original signal',
                  color = plot_config['color_original'], linewidth = plot_config['linewidth_original'])
@@ -194,7 +201,7 @@ for n_plot in range(plot_to_create):
     ax_freq.set_xlabel("Frequency [Hz]", fontsize = plot_config['fontsize'])
     ax_freq.set_ylabel(r"PSD [$\mu V^2/Hz$]", fontsize = plot_config['fontsize'])
     ax_freq.set_xlim([0, fs/2])
-    # ax_freq.legend()
+    ax_freq.legend()
     ax_freq.grid(True)
     ax_freq.tick_params(axis = 'both', labelsize = plot_config['fontsize'])
 
@@ -215,9 +222,17 @@ for n_plot in range(plot_to_create):
         # path_save += "subj_{}_trial_{}_ch_{}_rep_{}_epoch_{}_label_{}".format(subj, n_trial + 1, channel, repetition, epoch, label_name)
         path_save += "{}_trial_{}_{}_epoch_{}".format(filename, n_trial + 1, channel, epoch)
 
+        if filter_config['use_filter'] : path_save += 'low_pass_filter'
+
         if use_test_set: path_save += '_test_set'
         else: path_save += '_train_set'
 
         for format in plot_config['format_so_save']:
             fig_time.savefig(path_save + "_time." + format, format = format)
             fig_freq.savefig(path_save + "_freq." + format, format = format)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+if rand_trial_sample : plt.close('all')
+
+
